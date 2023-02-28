47b8b9b102e042a2fc01007a323407d3293d39c0
Marcos Pinheiro <viniciusm896@gmail.com>
2023-02-28 19:18:35 -0300

    Login, Logout e Cadastro de Usuários
    - Implementado Cadastro de Usuários
    
    - Adicionado funcionalidade de efetuar Login/Logout

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController {
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->except('_token');
+        $data['password'] = Hash::make($data['password']);
+        User::create($data);
+    }
+}
