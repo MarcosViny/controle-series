113c94261f0109ddae1292897053297c7dd72424
Marcos Pinheiro <viniciusm896@gmail.com>
2024-03-02 12:48:35 -0300

    Modificações API - método de criar série e método de obter série específica
    - Ajustes realizados para cadastrar série utilizando o serviço de repository (SeriesRepository), dessa forma utilizando as regras de negócio já estabelecidas;
    
    - Criado método para pegar uma série específica, de acordo com o id da série indicado (método show);
    
    - Eager Loading de vários relacionamentos para obter uma série com suas temporadas e episódios;

--- app/Http/Controllers/Api/SeriesController.php
+++ app/Http/Controllers/Api/SeriesController.php
@@ -4,16 +4,31 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Routing\Controller;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository) {}
+
     public function index() {
         return Series::all();
     }
 
     public function store(SeriesFormRequest $request) {
         return response()
-            ->json(Series::create($request->all()), 201);
+            ->json($this->seriesRepository->add($request), 201);
     }
+
+    public function show(Series $series) {
+        return $series;
+    }
+
+    // Obter uma série com suas temporadas e episódios
+    /* public function show(int $series) {
+        // Utilizando Eager Loading
+        return Series::whereId($series)
+            ->with('seasons.episodes')
+            ->first();
+    } */
 }
